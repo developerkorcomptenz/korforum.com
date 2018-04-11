1b99eeabaf66186ab9c5cc72268d0162fd412f27
Boopathi <RajanB@korcomptenz.com>
2018-04-11 15:56:02 +0530

    Update vendor files and fix file uploader issue

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -96,7 +96,7 @@ return array (
   'kartik-v/yii2-widget-fileinput' => 
   array (
     'name' => 'kartik-v/yii2-widget-fileinput',
-    'version' => '9999999-dev',
+    'version' => '1.0.6.0',
     'alias' => 
     array (
       '@kartik/file' => $vendorDir . '/kartik-v/yii2-widget-fileinput',
